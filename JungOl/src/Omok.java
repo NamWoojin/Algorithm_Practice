4c512fc4ab1bc64d6a63c22cd5025e7860cf5252
NamWoojin <47585253+NamWoojin@users.noreply.github.com>
2020-07-27 12:34:43 +0900

    오목 수정 및 통과

--- JungOl/src/Omok.java
+++ JungOl/src/Omok.java
@@ -1,55 +1,68 @@
 import java.util.Scanner;
 
 public class Omok {
-	static int[] dr = {-1,-1,0,1,1,1,0,-1};
-	static int[] dc = {0,1,1,1,0,-1,-1,-1};
+	static int[] dr = { -1, -1, 0, 1, 1, 1, 0, -1 };
+	static int[] dc = { 0, 1, 1, 1, 0, -1, -1, -1 };
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = 19;
 		int[][] board = new int[N][N];
-		for(int i = 0; i<N;++i) {
-			for(int j = 0; j<N;++j) {
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
 				board[i][j] = sc.nextInt();
 			}
 		}
 		boolean endFlag = false;
-		for(int i =0; i<N;++i) {
-			for(int j = 0; j<N;++j) {
-				if(board[i][j] != 0) {
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				if (board[i][j] != 0) {
 					int color = board[i][j];
-					for(int k = 0; k< 8; ++k) {
-						int r = i+ dr[k];
-						int c = j+ dc[k];
-						if(r<0||c<0||r>=N||c>=N) 
+					for (int k = 0; k < 8; ++k) {
+						int r = i + dr[k];
+						int c = j + dc[k];
+						if (r < 0 || c < 0 || r >= N || c >= N)
 							continue;
-						if(board[r][c] == color) {
+						if (board[r][c] == color) {
 							int count = 2;
-							for(int l = 0; l<4;++l) {
+							for (int l = 0; l < 4; ++l) {
 								r += dr[k];
 								c += dc[k];
-								if(board[r][c] != color) 
-									break;
+								if (!(r < 0 || c < 0 || r >= N || c >= N)) {
+									if (board[r][c] != color)
+										break;
+									else
+										++count;
+								}
 								else
-									++count;
+									break;
 							}
-							if(count==5) {
-								System.out.println(color);
-								System.out.println((i+1)+" "+(j+1));
-								endFlag = true;
-								break;
+							if (count == 5) {
+								int reR = i - dr[k];
+								int reC = i - dr[k];
+
+								if (!(reR < 0 || reC < 0 || reR >= N || reC >= N)) {
+									if (board[i - dr[k]][j - dc[k]] != color) {
+										System.out.println(color);
+										System.out.println((i + 1) + " " + (j + 1));
+										endFlag = true;
+										break;
+									}
+								}
+
 							}
 						}
 					}
-					
+
 				}
-				if(endFlag)
+				if (endFlag)
 					break;
 			}
-			if(endFlag)
+			if (endFlag)
 				break;
-			
+
 		}
-		if(!endFlag) {
+		if (!endFlag) {
 			System.out.println(0);
 		}
 		sc.close();
