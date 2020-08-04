e74c2934ca966a5a5a962c6e2943a5b48b94d866
NamWoojin <47585253+NamWoojin@users.noreply.github.com>
2020-08-04 22:26:41 +0900

    Update _3289.java

--- SWExpertAcademy/src/_3289.java
+++ SWExpertAcademy/src/_3289.java
@@ -7,30 +7,32 @@ import java.util.StringTokenizer;
 
 public class _3289 {
 	static int[] parents;
+	static int[] rank;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuffer sf = new StringBuffer();
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T =Integer.parseInt(in.readLine());
-		for(int tc = 1; tc<=T;++tc) {
-			StringTokenizer st = new StringTokenizer(in.readLine()," ");
+		int T = Integer.parseInt(in.readLine());
+		for (int tc = 1; tc <= T; ++tc) {
+			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
 			int n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
-			parents =new int[n];
-			for(int i =0; i<n;++i) {
+			parents = new int[n];
+			rank = new int[n];
+			for (int i = 0; i < n; ++i) {
 				parents[i] = i;
 			}
 			sf.append("#").append(tc).append(" ");
-			for(int i =0;i<m;++i) {
-				st = new StringTokenizer(in.readLine()," ");
+			for (int i = 0; i < m; ++i) {
+				st = new StringTokenizer(in.readLine(), " ");
 				int num = Integer.parseInt(st.nextToken());
-				int a = Integer.parseInt(st.nextToken())-1;
-				int b = Integer.parseInt(st.nextToken())-1;
-				if(num == 1){
-					sf.append(Equal(a,b));
-				}
-				else {
-					union(a,b);
+				int a = Integer.parseInt(st.nextToken()) - 1;
+				int b = Integer.parseInt(st.nextToken()) - 1;
+				if (num == 1) {
+					sf.append(Equal(a, b));
+				} else {
+					union(a, b);
 				}
 			}
 			out.write(sf.toString());
@@ -39,27 +41,38 @@ public class _3289 {
 			sf.setLength(0);
 		}
 	}
+
 	static int Equal(int a, int b) {
-		if(a == b)
+		if (a == b)
 			return 1;
-		if(find(a) == find(b))
+		if (find(a) == find(b))
 			return 1;
 		return 0;
 	}
+
 	static void union(int a, int b) {
-		if(a != b) {
-			int pa =find(a);
-			int pb =find(b);
-			if(pa != pb) {
-				parents[pb] = pa;
+		if (a != b) {
+			int pa = find(a);
+			int pb = find(b);
+			if (pa != pb) {
+				if (rank[pa] > rank[pb])
+					parents[pb] = pa;
+				else if (rank[pa] < rank[pb])
+					parents[pa] = pb;
+				else {
+					parents[pa] = pb;
+					rank[pb]++;
+				}
 			}
+
 		}
 	}
+
 	static int find(int x) {
-		if(parents[x]==x) {
+		if (parents[x] == x) {
 			return x;
 		}
 		return find(parents[x]);
 	}
-	
+
 }
