3393c8f213f7cb72ff942461b7b8527029c890e1
NamWoojin <47585253+NamWoojin@users.noreply.github.com>
2020-09-11 17:25:34 +0900

    Update _5656_벽돌깨기.java

--- SWExpertAcademy/src/_5656_벽돌깨기.java
+++ SWExpertAcademy/src/_5656_벽돌깨기.java
@@ -7,9 +7,9 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class _5656_벽돌깨기 {
-	static int[][] array, tempArray;
-	static int[] top, tempTop;
-	static int N, W, H,all, max = 0;
+	static int[][] array, tempArray[];
+	static int[] top, tempTop[];
+	static int N, W, H,all, min = 0;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
@@ -30,38 +30,53 @@ public class _5656_벽돌깨기 {
 			N = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 			H = Integer.parseInt(st.nextToken());
+			tempArray = new int[N][][];
+			tempTop = new int[N][];
 			array = new int[H][W];
 			top = new int[W];
+			boolean[] findTop = new boolean[W];
 			all = 0;
+			min = Integer.MAX_VALUE;
+			Arrays.fill(top, H);
 			for (int i = 0; i < H; ++i) {
 				st = new StringTokenizer(in.readLine());
 				for (int j = 0; j < W; ++j) {
 					array[i][j] = Integer.parseInt(st.nextToken());
 					if(array[i][j]!=0)
 						++all;
-					if (top[j] == 0 && array[i][j] != 0)
+					if (!findTop[j] && array[i][j] != 0) {
 						top[j] = i;
+						findTop[j] = true;
+					}
 				}
 			}
 			dfs(0,0);
-			System.out.println(max);
+			System.out.println("#"+tc+" "+min);
 		}
 	}
 
 	private static void dfs(int count, int sum) {
 		if (count == N) {
-			
-			max = Math.max(all-sum, max);
+//			System.out.println(all-sum);
+			min = Math.min(all-sum, min);
 			return;
 		}
 
+		if(min == 0)
+			return;
+		
+		if(sum == all) {
+			min = 0;
+			return;
+		}
+		
 		for (int i = 0; i < W; ++i) {
-			if (top[i] != 0) {
-				copying();
+			if (top[i] != H) {
+				copying(count);
 				int num = breaking(top[i], i);
 				align();
 				dfs(count+1,sum+num);
-				recovering();
+				recovering(count);
 			}
 		}
 	}
@@ -76,6 +91,7 @@ public class _5656_벽돌깨기 {
 			Node n = queue.poll();
 			int num = array[n.r][n.c];
 			array[n.r][n.c] = 0;
+			++top[n.c];
 			++count;
 			for (int k = 0; k < 4; ++k) {
 				int cnt = 0;
@@ -88,6 +104,8 @@ public class _5656_벽돌깨기 {
 					if(rr<0||cc<0||rr>=H||cc>=W)
 						break;
 					if(visited[rr][cc])
+						continue;
+					if(array[rr][cc] == 0)
 						continue;
 					
 					visited[rr][cc] = true;
@@ -106,14 +124,13 @@ public class _5656_벽돌깨기 {
 			int down = H-1;
 			int up= H-1;
 			boolean find = false;
-			for(int j = H-1;j>0;--j) {
+			for(int j = H-1;j>=0;--j) {
 				if(array[j][i] == 0) {
 					find = true;
 				}else {
 					if(find){
 						array[down][i] = array[up][i];
 						array[up][i] = 0;
-						--top[i];
 					}
 					--down;
 				}
@@ -123,19 +140,21 @@ public class _5656_벽돌깨기 {
 //		for(int i = 0; i<H;++i)
 //			System.out.println(Arrays.toString(array[i]));
 //		System.out.println();
+//		System.out.println(Arrays.toString(top));
+//		System.out.println();
 	}
 
-	private static void copying() {
-		tempArray = new int[H][];
+	private static void copying(int idx) {
+		tempArray[idx] = new int[H][];
 		for (int j = 0; j < array.length; ++j) {
-			tempArray[j] = array[j].clone();
+			tempArray[idx][j] = array[j].clone();
 		}
-		tempTop = top.clone();
+		tempTop[idx] = top.clone();
 	}
 
-	private static void recovering() {
-		array = tempArray;
-		top = tempTop;
+	private static void recovering(int idx) {
+		array = tempArray[idx];
+		top = tempTop[idx];
 	}
 
 }
