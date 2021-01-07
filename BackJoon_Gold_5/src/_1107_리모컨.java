bf6b7fe2b54e22ee15d8abe0b36da7cf1777865c
NamWoojin <nam990304@gmail.com>
2021-01-07 15:10:04 +0900

    Update _1107_리모컨.java

--- BackJoon_Gold_5/src/_1107_리모컨.java
+++ BackJoon_Gold_5/src/_1107_리모컨.java
@@ -4,14 +4,23 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _1107_리모컨 {
+	static StringBuilder bigger = new StringBuilder(), smaller = new StringBuilder();
+	static int count = 0;
+	static int min = 0, max = 0;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(in.readLine());
 		int M = Integer.parseInt(in.readLine());
-		boolean[] broken = new boolean[12];
+		boolean[] broken = new boolean[10];
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		for (int i = 0; i < M; ++i) {
-			
+			broken[Integer.parseInt(st.nextToken())] = true;
 		}
+		count = Math.abs(100 - N);
+		min = N;
+		max = N;
 	}
+	
+	
+	
 }
